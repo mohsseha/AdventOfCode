9e70aac6664a57aa8f08d3acd763ce54811191be
service account GCP Builder <service_account@husain.io>
2022-07-11 12:54:39 +0000

    fixed most common word, not impressive but correct scaling, can you do better?

--- LeetCode/most-common-word.py
+++ LeetCode/most-common-word.py
@@ -1,16 +1,46 @@
-# NOT DONE! WILL BE CONTINUED IN NEXT CLASS: https://leetcode.com/problems/most-common-word/
+# naive implementation  https://leetcode.com/problems/most-common-word/
 
 
 from typing import List
 
 
 class Solution:
-    SPLIT_CHARS="!?',;."
+    SPLIT_CHARS=set(" !?',;.") #set is minor optimization
+
+    def par_to_word_list(self,paragraph:str)-> List[str]:
+        res=[]
+        word=""
+        for i,c in enumerate(paragraph):
+            if c in self.SPLIT_CHARS or i==len(paragraph)-1:
+                if i==len(paragraph)-1 and c not in self.SPLIT_CHARS:
+                    word+=c
+                if word:
+                    res.append(word)
+                word=""
+                continue
+            word+=c
+        return res
+
+
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
         paragraph=paragraph.lower()
-        banned=[b.lower() for b in banned]
+        banned=set(b.lower() for b in banned)
+        word_list=self.par_to_word_list(paragraph)
+        hist={}
+        for w in word_list:
+            if w in banned:
+                continue
+            if w in hist:
+                hist[w]=hist[w]+1
+            else:
+                hist[w]=1
+        mx_word=list(hist.keys())[0]
+        for w in hist.keys():
+            if hist[w]>hist[mx_word]:
+                mx_word=w
+        return mx_word
         
-
+        
 
 
 
